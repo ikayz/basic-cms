1fcb60a5e60381c55b7584507d55d3d8f730ccc3
Isaac Miti <ikayz@live.com>
2018-11-27 12:18:56 +0200

    Initialized repo with basic structure

---
+++ public/index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+
+<html lang="en">
+  <head>
+    <title>Basic CMS</title>
+    <meta charset="utf-8">
+  </head>
+
+  <body>
+   <h1>Basic CMS</h1>
+  </body>
+</html>
---
+++ public/staff/index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+
+<html lang="en">
+  <head>
+    <title>Basic CMS</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <header>
+    </header>
+
+    <nav>
+
+    </nav>
+
+    <footer>
+    </footer>
+  </body>
+</html>
